e9c1f6640f9cbc71f5a91f8fcdcbf998ea729c4a
github username <github email address>
2018-02-17 18:34:35 -0800

    refactored solution, added brute force solution

--- product_excluding_index.rb
+++ product_excluding_index.rb
@@ -1,10 +1,20 @@
-def get_products_except_index(int_array)
-  product_before_index = get_products_before_index(int_array)
-  
-  product_after_index = get_products_after_index(int_array)
-  
-  return product_before_index.collect.with_index {|product_before, index| product_before * product_after_index[index]  }
+# Brute Force Solution - O(n^2)
+def get_products_no_index(int_arr)
+  results = []
+  int_arr.each_with_index do |int_a, index_a|
+    product = 1
+    int_arr.each_with_index do |int_b, index_b|
+      next if index_b == index_a
+      product *= int_b
+    end
+    results.push(product)
+  end
+  results
 end
+
+
+# Most effcient solution - O(n)
+# Greedy approach
 
 def get_products_before_index(int_array)
   product_so_far = 1
@@ -19,28 +29,17 @@ def get_products_before_index(int_array)
   all_products_before
 end
 
-def get_products_after_index(int_array)
-  product_so_far = 1
-  all_products_after = []
-  index =  int_array.length - 1
-  
-  while index >= 0
-    all_products_after[index] = product_so_far
-    product_so_far *= int_array[index]
-    index -= 1
-  end
-  all_products_after
-end
-
-
-
-def efficient_products(int_array)
-  return [] in int_array.length == 1
+def efficient_products_no_index(int_array)
+  return [] in if int_array.length == 1
+  # anticipates edge case of array of length 1
   results = []
   products_before_index = get_products_before_index(int_array)
+  # get array of products before index, push them into an array
   product_so_far = 1
   index = int_array.length - 1
   while index >= 0
+    # iterate through product array backwards to calculate 
+    # the products except for the index
     results[index] = product_so_far * products_before_index[index]
     product_so_far *= int_array[index]
     index -= 1
@@ -48,5 +47,3 @@ def efficient_products(int_array)
   results
 end
 
-get_products_after_index([1,2,6,5,9])
-efficient_products([1,2,6,5,9])
