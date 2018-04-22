b2c96c3ab47dc9dbbf87eac9afda2286887e3726
Alexandra <github email address>
2018-04-22 10:32:43 -0700

    added arr_jumper.rb solution, restructured to use tests

---
+++ solutions/README.md
@@ -0,0 +1,2 @@
+# ruby_interview_algorithm_practice
+I am working through a compendium of algorithm and data structure questions that are frequently asked in "Big N" Software company interviews.
---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require "bundler/setup"
+require "pry"
+require "require_all"
+require_all "solutions"
+
+
+RSpec.configure do |config|
+
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html,
+                                    # :json, CustomFormatterClass
+
+end
