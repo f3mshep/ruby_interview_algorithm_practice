4deadf81d53dd3d99ee75cd752ffae126232fc9d
f3mshep <superbiscuit@gmail.com>
2018-04-23 17:21:06 -0700

    #length_of_longest_substring is working

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ruby_interview_algorithm_practice
 I am working through a compendium of algorithm and data structure questions that are frequently asked in "Big N" Software company interviews.
 
-#
+## Usage:
+Clone the repo, and delete or rename solutions folder. Write code to make the rspec tests pass!
---
+++ spec/longest_substring_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+describe "#length_of_longest_substring" do
+  it "returns the length of the longest substring" do
+    expect(length_of_longest_substring('abcabcbb')).to eq(3)
+    expect(length_of_longest_substring('bbbbb')).to eq(1)
+    expect(length_of_longest_substring('pwwkew')).to eq(3)
+    expect(length_of_longest_substring('c')).to eq(1)
+    expect(length_of_longest_substring('dvdf')).to eq(3)
+    expect(length_of_longest_substring('au')).to eq(2)
+  end
+end
