a0443d3c62009cf0266ac677a9d4e26f2240a103
Alexandra <github email address>
2018-04-27 11:56:50 -0700

    added specs for cycle detection

--- solutions/linked_list_contains_cycle.rb
+++ solutions/linked_list_contains_cycle.rb
@@ -5,22 +5,10 @@ class LinkedListNode
   attr_accessor :value, :next
 
   def initialize(value)
-    @value = value
-    @next  = nil
+    self.value = value
+    self.next  = nil
   end
 end
-
-a = LinkedListNode.new('A')
-b = LinkedListNode.new('B')
-c = LinkedListNode.new('C')
-d = LinkedListNode.new('D')
-e = LinkedListNode.new('E')
-f = LinkedListNode.new('F')
-
-a.next = b
-b.next = c
-c.next = d
-d.next = c
 
 # We can reduce space here
 def unoptimized_contains_cycle(head)
@@ -51,9 +39,8 @@ def contains_cycle(head)
       slow_node = slow_node.next
     end
     count += 1
-    return true if slow_node == fast_node 
+    return true if slow_node == fast_node
   end
   false
 end
 
-puts contains_cycle(a)
