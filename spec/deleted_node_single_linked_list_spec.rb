0708518d86dc6ec259123c39bad4097fc1e341bd
f3mshep <superbiscuit@gmail.com>
2018-05-02 17:08:54 -0700

    rspec completed for #delete_node

--- spec/deleted_node_single_linked_list_spec.rb
+++ spec/deleted_node_single_linked_list_spec.rb
@@ -1,5 +1,12 @@
 require "spec_helper"
 
+# If you are reading this, and scratching your head, it is because you can't
+# represent a linked list as well without using memory pointers
+# a node in a classic linked list contains a reference to the next node
+# as a memory pointer. That is why we accept changing the value
+# think of it more like changing the memory pointer to the next element in the
+# linked list
+
 describe "#delete_node" do
 
   before :each do
@@ -12,7 +19,12 @@ describe "#delete_node" do
   end
 
   it "deletes a node from the middle of a linked list" do
+    delete_node(@b)
+    expect(@b.value).to eq('C')
   end
   it "deletes a node from the end of a linked list" do
+    delete_node(@c)
+    expect(@c.value).to eq(nil)
   end
+
 end
