954a73cc2e884874f0bcd8e1501feec17dc99dd9
Alexandra <github email address>
2018-06-20 19:10:48 -0700

    added solution for #string_decoder

---
+++ solutions/string_decoder.rb
@@ -0,0 +1,24 @@
+def decode_string(str)
+  int_arr = []
+  chunk = ""
+  res = []
+  str.each_char do |char|
+    if char == "[" && !chunk.empty?
+        res.push(chunk)
+        chunk = ""
+    elsif char == "]" && chunk.empty?
+        times = int_arr.pop
+        res.push(chunk * times)
+        chunk = ""
+    elsif char.to_i != 0
+      int_arr.push(char.to_i)
+    elsif letter?(char)
+      chunk += char
+    end
+  end
+  res.join * int_arr.pop
+end
+
+def letter?(char)
+  char =~ /[A-Za-z]/
+end
