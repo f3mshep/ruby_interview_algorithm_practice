2b32e9261eb8a7ea508a9c4f93c72b846051fa55
f3mshep <superbiscuit@gmail.com>
2018-07-05 11:34:30 -0700

    added solution to #is_valid_ip?

--- spec/valid_id_spec.rb
+++ spec/valid_id_spec.rb
@@ -8,6 +8,6 @@ describe "#is_valid_ip?" do
     expect(is_valid_ip?('255.255.255.255')).to eq(true)
   end
   it 'returns false if there are an incorrect amount of octets' do
-    expect(is_valid_ip?('13.2'))to eq(false)
+    expect(is_valid_ip?('13.2')).to eq(false)
   end
 end
