5f08ce1d7282c0219c775948ded12aef6f22f2be
f3mshep <superbiscuit@gmail.com>
2018-07-09 11:51:37 -0700

    added solution for #can_make_valid_palindrome

--- solutions/can_make_valid_palindrome.rb
+++ solutions/can_make_valid_palindrome.rb
@@ -2,5 +2,25 @@
 # whether you can make it a palindrome.
 
 def can_make_valid_palindrome(s)
+  is_palindrome_helper?(s)
+end
 
+def is_palindrome_helper?(s, removed = false, coords = nil)
+  if coords
+    left, right = coords.first, coords.last
+  else
+    left, right = 0, s.length - 1
+  end
+  while left < right
+    if s[left] == s[right]
+      left +=1
+      right -=1
+    elsif removed
+      return false
+    else
+      return true if is_palindrome_helper?(s, true, [left + 1, right])
+      return is_palindrome_helper?(s, true, [left, right - 1])
+    end
+  end
+  true
 end
