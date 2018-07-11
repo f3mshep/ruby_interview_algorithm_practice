d15f1125cb100c22a33d35ca5bf834d7acdc6d30
f3mshep <superbiscuit@gmail.com>
2018-07-11 14:09:34 -0700

    added spec

--- spec/min_stack_spec.rb
+++ spec/min_stack_spec.rb
@@ -3,23 +3,47 @@ require "spec_helper"
 describe "MinStack" do
   describe "#stack" do
     it "exposes the stack" do
+      test_stack = MinStack.new
+      test_stack.stack = [1,2,3]
+      expect(test_stack.stack).to eq([1,2,3])
     end
   end
   describe "#push" do
     it "pushes an element onto the stack" do
-      
+      test_stack = MinStack.new
+      test_stack.push(1)
+      test_stack.push(2)
+      expect(test_stack.stack).to eq([1,2])
     end
   end
   describe "#top" do
     it "returns the top element of the stack" do
-    end
-  end
-  describe "#get_min" do
-    it "returns the smallest element" do
+      test_stack = MinStack.new
+      test_stack.push(1)
+      test_stack.push(2)
+      expect(test_stack.top).to eq(2)
     end
   end
   describe "pop" do
     it "removes the top element and returns it" do
+      test_stack = MinStack.new
+      test_stack.push(1)
+      test_stack.push(2)
+      test_stack.pop
+      expect(test_stack.stack).to eq([1])
     end
   end
+  describe "#get_min" do
+    it "returns the smallest element" do
+      test_stack = MinStack.new
+      test_stack.push(1)
+      test_stack.push(2)
+      expect(test_stack.get_min).to eq(1)
+      test_stack.push(-1)
+      expect(test_stack.get_min).to eq(-1)
+      test_stack.pop
+      expect(test_stack.get_min).to eq(1)
+    end
+  end
+
 end
