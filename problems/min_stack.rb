70ac22b7b5f42a772606670072cd50b4539a14e2
f3mshep <superbiscuit@gmail.com>
2018-07-11 14:01:02 -0700

    working on #MinStack

---
+++ problems/min_stack.rb
@@ -0,0 +1,31 @@
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+# push(x) -- Push element x onto stack.
+# pop() -- Removes the element on top of the stack.
+# top() -- Get the top element.
+# getMin() -- Retrieve the minimum element in the stack.
+
+class MinStack
+
+  def initialize
+    # initialize MinStack
+  end
+
+  def pop
+    # remove the top element of the stack and return it
+
+  end
+
+  def push
+    # push element on to top of stack
+  end
+
+  def top
+    # return the top element of the stack
+  end
+
+  def get_min
+    # return the smallest element in the stack
+  end
+
+end
---
+++ spec/min_stack_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe "MinStack" do
+  describe "#stack" do
+    it "exposes the stack" do
+    end
+  end
+  describe "#push" do
+    it "pushes an element onto the stack" do
+      
+    end
+  end
+  describe "#top" do
+    it "returns the top element of the stack" do
+    end
+  end
+  describe "#get_min" do
+    it "returns the smallest element" do
+    end
+  end
+  describe "pop" do
+    it "removes the top element and returns it" do
+    end
+  end
+end
