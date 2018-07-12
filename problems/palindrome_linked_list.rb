fcb3adaed746f6af76142e99b0d10af4bb122806
f3mshep <superbiscuit@gmail.com>
2018-07-12 10:11:56 -0700

    added solution for #palindrome_linked_list

---
+++ problems/palindrome_linked_list.rb
@@ -0,0 +1,14 @@
+# Given a singly linked list, determine if it is a palindrome.
+# solution must be in O(1) space and O(n) time
+
+# Example 1:
+# Input: 1->2
+# Output: false
+
+# Example 2:
+# Input: 1->2->2->1
+# Output: true
+
+def palindrome_linked_list(head)
+
+end
---
+++ spec/palindrome_linked_list_spec.rb
@@ -0,0 +1,52 @@
+require "spec_helper"
+
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val)
+        @val = val
+        @next = nil
+    end
+end
+
+describe "#palindrome_linked_list" do
+  it "returns true if linked list is a palindrome" do
+    a = ListNode.new(1)
+    b = ListNode.new(2)
+    c = ListNode.new(2)
+    d = ListNode.new(1)
+    a.next = b
+    b.next = c
+    c.next = d
+    g = ListNode.new(1)
+    h = ListNode.new(1)
+    i = ListNode.new(2)
+    j = ListNode.new(1)
+    k = ListNode.new(1)
+    g.next = h
+    h.next = i
+    i.next = j
+    j.next = k
+    expect(palindrome_linked_list(a)).to eq(true)
+    expect(palindrome_linked_list(g)).to eq(true)
+  end
+  it "returns false if linked list is not a palindrome" do
+    a = ListNode.new(1)
+    b = ListNode.new(2)
+    c = ListNode.new(3)
+    d = ListNode.new(4)
+    e = ListNode.new(5)
+    a.next = b
+    b.next = cr
+    c.next = d
+    d.next = e
+    f = ListNode.new(1)
+    g = ListNode.new(2)
+    h = ListNode.new(3)
+    i = ListNode.new(4)
+    f.next = g
+    g.next = h
+    h.next = i
+    expect(palindrome_linked_list(a)).to eq(false)
+    expect(palindrome_linked_list(f)).to eq(false)
+  end
+end
